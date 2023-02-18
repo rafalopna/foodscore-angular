ec3a77334e4dbcfefd542e49102a571d4d552ac6
Rafa López <rafalopez.nv@gmail.com>
2023-02-18 20:40:32 +0100

    Modify -> eslint

--- src/app/shared/validators/password-match.validator.ts
+++ src/app/shared/validators/password-match.validator.ts
@@ -1,6 +1,6 @@
 import { ValidatorFn, AbstractControl } from '@angular/forms';
 
-export const passwordMatchValidator: ValidatorFn = (control: AbstractControl): {[key: string]: any} | null => {
+export const passwordMatchValidator: ValidatorFn = (control: AbstractControl): {[key: string]: unknown} | null => {
   const password1 = control.get('password1')?.value;
   const password2 = control.get('password2')?.value;
   return password1 === password2 ? null : { passwordMatch: { value: control.value } };
